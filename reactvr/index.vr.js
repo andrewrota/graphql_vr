504b18a63f574bffda952ddbb189b84bec9d12d7
Andrew Rota <rota.andrew@gmail.com>
2017-02-10 20:32:52 -0500

    Update components

--- reactvr/index.vr.js
+++ reactvr/index.vr.js
@@ -10,39 +10,52 @@ import {
   View,
   } from 'react-vr';
 
-class reactvr extends React.Component {
-  _onViewClicked = (name) => {
-    console.log(name);
-  }
-
+class Nodes extends React.Component{
   render() {
-    const nodes = [
-      {name: 'foo'}, {name: 'bar'}, {name: 'baz'}, {name: 'test'}, {name: 'root'}
-    ];
     return (
       <View>
-        <Pano source={asset('eso0932a.jpg')}/>
-        {nodes.map((item, i) =>
-            <VrButton
-              key={item.name}
-              style={{layoutOrigin: [0.5, 0.5, 0],
+        {this.props.nodes.map((item, i) =>
+        <VrButton
+          key={item.name}
+          style={{layoutOrigin: [0.5, 0.5, 0],
               position: 'absolute',
               width: 1,
               height: 1,
-              transform: [{rotateY: i*(360/nodes.length)},
+              transform: [{rotateY: i*(360/this.props.nodes.length)},
               {translate: [0, 0, -5]}]}}
-              onClick={()=>this._onViewClicked(item.name)}>
-              <View
-                key={item.name}
-                style={{ margin: 0.1, height: 1, width: 1, backgroundColor: 'red'}}>
-                <Text style={{fontSize: 0.2, textAlign: 'center'}}>{item.name}</Text>
-              </View>
-            </VrButton>
+          onClick={()=>this.props.onClick(item.name)}>
+          <View
+            key={item.name}
+            style={{ margin: 0.1, height: 1, width: 1, backgroundColor: 'red'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>{item.name}</Text>
+          </View>
+        </VrButton>
         )}
       </View>
     );
   }
 }
-;
+const nodes = [{name: 'foo', nodes: [{name: 'childFoo'}, {name: 'childBar'}, {name: 'childBaz'}]}, {name: 'bar'}, {name: 'baz'}, {name: 'test'}, {name: 'root'}];
+class reactvr extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {nodes: []};
+  }
+  componentDidMount() {
+    this.setState({nodes});
+  }
+  onClick = (name) => {
+    //console.log(this.nodes.find((el) => {el.name === name}));
+    //this.setState({nodes: nodes.find((el) => {el.name === name})});
+  }
+  render() {
+    return (
+      <View>
+        <Pano source={asset('eso0932a.jpg')}/>
+        <Nodes onClick={this.onClick} nodes={this.state.nodes} />
+      </View>
+    );
+  }
+}
 
 AppRegistry.registerComponent('reactvr', () => reactvr);
