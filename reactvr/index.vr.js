a65444c7ad9dcacc1287edb3920b812681e4b412
Andrew Rota <rota.andrew@gmail.com>
2017-02-10 19:52:23 -0500

    Space nodes out in sphere

--- reactvr/index.vr.js
+++ reactvr/index.vr.js
@@ -4,27 +4,25 @@ import {
   asset,
   StyleSheet,
   Pano,
+  VrButton,
+  Image,
   Text,
   View,
 } from 'react-vr';
 
 class reactvr extends React.Component {
   render() {
+    const nodes = [
+      {name: 'foo'}, {name: 'bar'}, {name: 'baz'}, {name: 'test'}, {name: 'root'}
+    ];
     return (
       <View>
         <Pano source={asset('space.jpg')}/>
-        <Text
-          style={{
-            backgroundColor:'blue',
-            padding: 0.02,
-            textAlign:'center',
-            textAlignVertical:'center',
-            fontSize: 0.8,
-            layoutOrigin: [0.5, 0.5],
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          graphQL
-        </Text>
+        {nodes.map((item, i) =>
+            <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red',transform: [{rotateY: i*(360/nodes.length)}, {translate: [0, 0, -6]}]}}>
+              <Text style={{fontSize: 0.2, textAlign: 'center'}}>{item.name}</Text>
+            </View>
+        )}
       </View>
     );
   }
