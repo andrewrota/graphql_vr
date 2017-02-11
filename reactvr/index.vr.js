07e48409dc3d97d5aa61daa5f86f06c4be8eebbc
Andrew Rota <rota.andrew@gmail.com>
2017-02-10 20:07:56 -0500

    Fix positioning around sphere

--- reactvr/index.vr.js
+++ reactvr/index.vr.js
@@ -14,6 +14,7 @@ class reactvr extends React.Component {
   _onViewClicked = (name) => {
     console.log(name);
   }
+
   render() {
     const nodes = [
       {name: 'foo'}, {name: 'bar'}, {name: 'baz'}, {name: 'test'}, {name: 'root'}
@@ -22,15 +23,18 @@ class reactvr extends React.Component {
       <View>
         <Pano source={asset('eso0932a.jpg')}/>
         {nodes.map((item, i) =>
-          <VrButton
-            key={item.name}
-            style={{width: 1, height: 1,transform: [{rotateY: i*(360/nodes.length)}, {translate: [0, 0, -6]}]}}
-            onClick={()=>this._onViewClicked(item.name)}>
-            <View
+            <VrButton
               key={item.name}
-              style={{ margin: 0.1, height: 1, width: 1, backgroundColor: 'red'}}>
-              <Text style={{fontSize: 0.2, textAlign: 'center'}}>{item.name}</Text>
-            </View>
+              style={{layoutOrigin: [0.5, 0.5, 0],
+              position: 'absolute',width: 1,
+              height: 1,transform: [{rotateY: i*(360/nodes.length)},
+              {translate: [0, 0, -5]}]}}
+              onClick={()=>this._onViewClicked(item.name)}>
+              <View
+                key={item.name}
+                style={{ margin: 0.1, height: 1, width: 1, backgroundColor: 'red'}}>
+                <Text style={{fontSize: 0.2, textAlign: 'center'}}>{item.name}</Text>
+              </View>
             </VrButton>
         )}
       </View>
